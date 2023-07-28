1f5b3b342b230283fa9e5a9a129ab13c68de2c44
jottahs <jottahs136@gmail.com>
2023-07-28 06:01:07 +0200

    Roles

--- resources/views/content/pages/pages-home.blade.php
+++ resources/views/content/pages/pages-home.blade.php
@@ -8,5 +8,22 @@ $configData = Helper::appClasses();
 
 @section('content')
 <h4>Home Page</h4>
-<h4>Cambios desde local</h4>
+<h4>Este contenido es público</h4>
+
+@role('superadministrador')
+    <p>Solo lo va a ver el rol Superadministrador</p>
+@endrole
+
+@role('administrador')
+    <p>Solo lo va a ver el rol Administrador</p>
+@endrole
+
+@role('JefeSucursal')
+    <p>Solo lo va a ver el rol Jefe de Sucursal</p>
+@endrole
+
+@role('Operador')
+    <p>Solo lo va a ver el rol de operador</p>
+@endrole
+
 @endsection
